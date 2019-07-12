768454ee78ba10345bd6cc27e80afa5b1b82d934
Kylagf2011 <kylagf2011@gmail.com>
2019-07-12 09:17:24 -0700

    Iteration 1 complete

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -14,7 +14,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func showMessage(sender: UIButton) {
+        //Get messages to show up in a random order when the button is pressed
+        let alertController = UIAlertController(title: "Good Work", message: "Well, that was good", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func showMessageTwo(sender: UIButton) {
+        //Get messages to show up in a random order when the button is pressed
+        let alertController = UIAlertController(title: "Funny, right", message: "Bee the best you you can be.", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
 
 }
 
